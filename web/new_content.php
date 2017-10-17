b058ef2115c7dc828cb067b2aca925b99f0f2a3a
andrii_kaa <kaapl@gmail.com>
2017-10-17 16:36:44 +0300

    les 12 respond with json data

--- web/new_content.php
+++ web/new_content.php
@@ -1,4 +1,9 @@
 <?php
-$var = 'new content';
+  $content = array(
+    'short' => 'New content',
+    'regular' => 'This is the new content which has been loaded by Ajax.',
+    'long' => 'This content is the result of making an Ajax query to a PHP page which dynamically generated text as a response.'
+  );
+
+  echo json_encode($content);
 ?>
-This is the <span style="color: mediumslateblue"><?=$var?></span> which has been loaded by Ajax.
